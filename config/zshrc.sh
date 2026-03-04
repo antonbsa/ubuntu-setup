fde5f6937665b36f6cc33fb25f829b919e63d1ad
Anton Barboza de Sá <antonbsa.bck@gmail.com>
2026-03-04 11:44:41 -0300

    update aliases file name and add zshrs.sh file

---
+++ config/zshrc.sh
@@ -0,0 +1,8 @@
+# keybinding to remove entire previous word by CTRL+Backspace 
+bindkey '^H' backward-kill-word
+
+# Codex variables for using vscode as external editor
+export VISUAL="code --wait"
+export EDITOR="code --wait"
+export GIT_EDITOR=nano
+export PATH="$HOME/.local/bin:$PATH"
